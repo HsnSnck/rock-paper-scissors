dcff5682b0d4aa2743776649ad887429975d1ff0
Hasan Sinecek <hasan.sinecek.dev@outlook.de>
2022-07-25 11:31:27 +0200

    on start plays 5 rounds, user have to input over prompt. simple input check

--- script.js
+++ script.js
@@ -43,17 +43,17 @@ function playRound(playerSelection, computerSelection) {
     }
   } else if (player === "paper") {
     if (computer === "scissor") {
-      return "You Lose! Paper beats Rock";
+      return "You Lose! Scissor beats Paper";
       
     } else if (computer === "rock") {
-      return "You Win! Rock beats Scissor";
+      return "You Win! Paper beats Rock";
       
     }
   } else if (player === "scissor") {
     if (computer === "rock") {
-      return "You Lose! Paper beats Rock";
+      return "You Lose! Rock beats Scissor";
     } else if (computer === "paper") {
-      return "You Win! Rock beats Scissor";
+      return "You Win! scissor beats Paper";
       
     }
   }
@@ -61,6 +61,36 @@ function playRound(playerSelection, computerSelection) {
   return "Error playRound() ";
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+//const playerSelection = "rock";
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
+
+
+var gameRounds=5;
+
+
+function game(){
+  for (let rounds = 0; rounds < gameRounds;gameRounds++) {
+    //ToDO check userinput
+    let playerSelection=prompt("Select between Rock, Paper, Scissor");
+    playerSelection=playerSelection.toLocaleLowerCase();
+    let inputTrue=checkPlayerInput(playerSelection);
+    while(!inputTrue){
+      playerSelection=prompt("Wrong Input. Please Select between Rock, Paper, Scissor");
+    }
+    let computerSelection=getComputerChoice();
+    console.log(playRound(playerSelection,computerSelection));
+    console.log("------------------------------------");
+  }
+}
+
+function checkPlayerInput(playerSelection){
+  switch (playerSelection){
+    case "rock" :console.log("You Selected Rock!");return true
+    case "paper":console.log("You Selected Paper!");return true
+    case "scissor":console.log("You Selected Scissor!");return true;
+    default:return false;
+  } 
+}
+
+game();
